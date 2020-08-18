f014c5e5a93e542589380a4afc0ab013dc77f404
lanak <Lanak-mu@mail.ru>
2020-08-18 17:15:04 +0300

    Rest adding images and run main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#import face_detection
-#import open_mvg_pipline
-#import open_mvs_pipline
+import face_detection
+import open_mvg_pipline
+import open_mvs_pipline
 
 
 
@@ -8,9 +8,9 @@ from parameter import *
 
 def main(requestId):
  print(requestId)
-  #  face_detection.detect_face(requestId)
-    #open_mvg_pipline.detect_sfm(requestId)
-    #open_mvs_pipline.detect_ply(requestId, 'sequential')
+    face_detection.detect_face(requestId)
+    open_mvg_pipline.detect_sfm(requestId)
+    open_mvs_pipline.detect_ply(requestId, 'sequential')
 
 if __name__ == '__main__':
     config = get_parameters()
